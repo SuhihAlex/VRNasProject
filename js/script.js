032c4fe7bed4c74aa6fdacf673f84cef6f8dd4b6
Alexandr Suhih <suhari93@mail.ru>
2024-04-06 20:43:11 +0300

    Добавил новую секцию

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-document.addEventListener('scroll', function() {
-      var elements = document.querySelectorAll('.anim-show');
-
-      for (var i = 0; i < elements.length; i++) {
-        var element = elements[i];
-        var position = element.getBoundingClientRect().top;
-        var windowHeight = window.innerHeight;
-
-        if (position < windowHeight) {
-          element.classList.add('show');
-        }
-      }
-    });
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      change.target.classList.add('element-show');
+    }
+  });
+}
+let options = { threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.block');
+for (let elm of elements) {
+  observer.observe(elm);
+}
